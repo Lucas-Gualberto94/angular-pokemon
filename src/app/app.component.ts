d4f96b7d471e0c308ba5159946a615cae56a7490
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2024-01-09 15:31:45 +0000

    requisition HTTP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, CardComponent],
+  template: `
+  <app-card></app-card>
+  <router-outlet></router-outlet>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
